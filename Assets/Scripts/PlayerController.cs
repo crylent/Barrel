1324a22810dce30c1104b175fecaf3bc94d74645
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2024-03-14 00:00:55 +0300

    Look around

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController: MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed = 0.05f;
+    [SerializeField] private Transform cameraArm;
+    [SerializeField] private Vector2 cameraRotationSpeed;
+    [SerializeField] private Vector2 cameraMaxDeviation;
+    [SerializeField] private Vector2 cameraReturnForce;
     [SerializeField] private GameObject barrel;
     [SerializeField] private GameObject destructionBarrel;
 
     private GameManager _gameManager;
     private Vector2 _movement;
-    private Camera _camera;
     private Animator _animator;
     private Animator _barrelAnimator;
     private static readonly int IsRunning = Animator.StringToHash("isRunning");
@@ -24,9 +27,10 @@ public class PlayerController: MonoBehaviour
     private void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
-        _camera = Camera.main;
         _animator = GetComponent<Animator>();
         _barrelAnimator = GetComponentsInChildren<Animator>()[1];
+        Input.multiTouchEnabled = true;
+        Debug.Log(Input.multiTouchEnabled);
     }
 
     private void Update()
@@ -36,13 +40,36 @@ public class PlayerController: MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_movement.magnitude == 0) return;
+        var cameraRot = SignedEulerAngles(cameraArm.localRotation.eulerAngles);
+        cameraArm.localRotation = Quaternion.Euler(Vector3.Scale(cameraRot, Vector2.one - cameraReturnForce));
         
+        if (_movement.magnitude == 0) return;
         var stickRotation = Mathf.Atan2(_movement.x, _movement.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(
             transform.rotation.eulerAngles.y + stickRotation * rotationSpeed,
             Vector3.up);
-        transform.Translate(speed * Time.fixedDeltaTime * Vector3.forward, _camera.transform);
+        transform.Translate(speed * Time.fixedDeltaTime * Vector3.forward);
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        var rot = Vector2.Scale(context.ReadValue<Vector2>().normalized, cameraRotationSpeed * Time.deltaTime);
+        var current = SignedEulerAngles(cameraArm.localRotation.eulerAngles);
+        var newX = current.x + rot.y;
+        var newY = current.y - rot.x;
+        cameraArm.localRotation = Quaternion.Euler(
+            Mathf.Abs(newX) <= cameraMaxDeviation.y ? newX : Mathf.Sign(newX) * cameraMaxDeviation.y,
+            Mathf.Abs(newY) <= cameraMaxDeviation.x ? newY : Mathf.Sign(newY) * cameraMaxDeviation.x,
+            0
+        );
+    }
+
+    private static Vector3 SignedEulerAngles(Vector3 eulerAngles)
+    {
+        if (eulerAngles.x > 180f) eulerAngles.x -= 360f;
+        if (eulerAngles.y > 180f) eulerAngles.y -= 360f;
+        if (eulerAngles.z > 180f) eulerAngles.z -= 360f;
+        return eulerAngles;
     }
     
     public void OnMove(InputAction.CallbackContext context)
